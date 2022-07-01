e09114e789c899714d7ae94b57b34e28ce81688c
Ayanbola <olaoluwaayanbola@gmail.com>
2022-07-01 03:39:33 +0100

    new

--- src/Pages/App.jsx
+++ src/Pages/App.jsx
@@ -3,15 +3,25 @@ import TopBar from '../Components/TopBar'
 import SideBar from "../Components/SideBar";
 import Home from "./Home";
 import '../Styles/App.scss'
+import  UserList from '../Pages/userList/UserList'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 function App() {
-  return (
-    <div className="App">
-      <TopBar></TopBar>
-      <div className="container">
-        <SideBar className="SideBar"></SideBar>
-        <Home className="Home">dnfgn</Home>
+  return( 
+    <Router>
+      <div className="App">
+        <TopBar></TopBar>
+        <div className="container">
+          <SideBar className="SideBar"></SideBar>
+          <Home></Home>
+          <Routes>
+            <Route path="/home">
+              <UserList></UserList>
+            </Route>
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import  '../Styles/Home.scss'
+import FeaturedInfo from './FeaturedInfo'
+import Chart from '../Components/Chart'
+import { userData } from '../Components/ChartData'
+
 const Home = () => {
   return (
     <div className='ConatinerHome'>
-        
+        <FeaturedInfo></FeaturedInfo>
+        <Chart data={userData} title="User Analytics" grid dataKey="Active User"/>
+        <div className="homeWidgets">
+
+       </div>
     </div>
   )
 }
